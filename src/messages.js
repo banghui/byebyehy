5909fd7aeabb42e401b7f72064ac0f1a0d54cc15
bang <limbanghui@gmail.com>
2020-07-29 21:58:49 +0800

    Fix corrupted images

--- src/messages.js
+++ src/messages.js
@@ -5,7 +5,7 @@ export const messages = [
     msg:
       "Keep in touch and hope to bake more food for you soon, and also to eat more of your baked goods, and also to travel again (to new york?).",
     coverImg: "./jingcover.jpg",
-    profileImg: "./jingprofile.jpg"
+    profileImg: "./jingprofile.jpg",
   },
   {
     name: "vern",
@@ -13,14 +13,14 @@ export const messages = [
     msg:
       "By learning the guitar. Hope that one day we can be friends, since we won’t be colleagues anymore.",
     coverImg: "./verncover.jpg",
-    profileImg: "./vernprofile.jpg"
+    profileImg: "./vernprofile.jpg",
   },
   {
     title: "If we try hard enough, we can do this! - Ex Web PM",
     name: "bui",
     msg: "Sayonara Master Chia T_T",
     coverImg: "./buicover.jpg",
-    profileImg: "./buiprofile.png"
+    profileImg: "./buiprofile.png",
   },
   {
     title: "scones connoisseur",
@@ -28,13 +28,13 @@ export const messages = [
     msg:
       "sup ninjasexybabe! congrats on your new role!! hopefully you will finally have your own Data Analyst in your team. come join us for CS:GO some time~",
     coverImg: "./weijiancover.jpg",
-    profileImg: "./weijianprofile.jpg"
+    profileImg: "./weijianprofile.jpg",
   },
   {
     title: "╳",
     name: "bang",
     msg: "",
-    coverImg: "./bang-cover.webp"
+    coverImg: "./bangcover.jpg",
   },
   {
     title: "Hello Huiyi,",
@@ -42,7 +42,7 @@ export const messages = [
     msg:
       "I'm thankful for all the kindness that you have treated me and the web team, from the first day I joined Carousell. On behalf of web team, we love you!",
     coverImg: "./trongcover.jpg",
-    profileImg: "./trongprofile.jpg"
+    profileImg: "./trongprofile.jpg",
   },
   {
     title: "Goodbye Madam Chia. It’s been real.",
@@ -50,14 +50,14 @@ export const messages = [
     msg:
       "Thanks for all the awesome memories and all the best to you. If you do need some fundings for your biz, please don’t hesitate to contact me at my new venture fund, J1000Capital.",
     coverImg: "./jaycover.jpg",
-    profileImg: "./jayprofile.jpg"
+    profileImg: "./jayprofile.jpg",
   },
   {
     title: "omorishoi",
     name: "sheng siong",
     msg:
       "Our meal from a year ago. Tons have changed since a year ago, let’s see how the next year turns out!!",
-    coverImg: "./sycover.jpg"
+    coverImg: "./sycover.jpg",
   },
   {
     title: "Goodbye Huiyi",
@@ -65,7 +65,7 @@ export const messages = [
     msg:
       "Still remember those good time when we were all together as a web team. All the best and take care  (•◡•) /",
     coverImg: "./andrewcover.jpg",
-    profileImg: "./andrewprofile.png"
+    profileImg: "./andrewprofile.png",
   },
   {
     title: "Ciao, master HuiYi",
@@ -73,14 +73,14 @@ export const messages = [
     msg:
       "Thank for inviting me to the durian meal. Wish you can visit TW in the future. Best wishes!",
     coverImg: "./ayaocover.jpg",
-    profileImg: "./ayaoprofile.jpg"
+    profileImg: "./ayaoprofile.jpg",
   },
   {
     title: "再见",
     name: "Kyle",
     msg: `“If you’re brave enough to say goodbye, life will reward you with a new hello.” – Paulo Coelho`,
     coverImg: "./mingcover.jpg",
-    profileImg: "./mingprofile.jpg"
+    profileImg: "./mingprofile.jpg",
   },
   {
     title: "Good things never last",
@@ -88,7 +88,7 @@ export const messages = [
     msg:
       "OOF~ as pessimistic as that might sound, your next job is going to have an end too. hah, good luck!",
     coverImg: "./ztcover.jpg",
-    profileImg: "./ztprofile.jpg"
+    profileImg: "./ztprofile.jpg",
   },
   {
     title: "SEE YOU ON THE PEAK OF ANOTHER MOUNTAIN",
@@ -96,7 +96,7 @@ export const messages = [
     msg:
       "Miss sitting beside youuuu :disappointed:  Now will miss you even moarr. Thanks for all the great memories! Wishing you all the best in your next adventure (waiting for Scones & Cream to IPO).",
     coverImg: "./yjcover.jpg",
-    profileImg: "./yjprofile.jpg"
+    profileImg: "./yjprofile.jpg",
   },
   {
     title: "Craigslist Numba 1",
@@ -105,7 +105,7 @@ export const messages = [
       "now that you don’t work there any more, am I allowed to uninstall Carousell from my phone?",
     coverImg: "./leecover.jpg",
     profileImg: "./leeprofile.jpg",
-    rel: "Ex-housemate"
+    rel: "Ex-housemate",
   },
   {
     title: "F45 master",
@@ -113,7 +113,7 @@ export const messages = [
     msg:
       "rmb to stay fit in your new season. don't let the scones get to you, or you'll be getting some muffins :zany_face:",
     coverImg: "./magcover.jpg",
-    profileImg: "./magprofile.png"
+    profileImg: "./magprofile.jpg",
   },
   {
     title: "Continue shine superwoman miss Huiyi",
@@ -121,7 +121,7 @@ export const messages = [
     msg:
       "It’s a pity that I never work with you. But I always amazed by your hardworking attitude. Thanks for bringing Carousell into another level. Let’s still keep in touch ❤️",
     coverImg: "./tinacover.jpg",
-    profileImg: "./tinaprofile.jpg"
+    profileImg: "./tinaprofile.jpg",
   },
   {
     title: "Best wishes for your next journey",
@@ -129,7 +129,7 @@ export const messages = [
     msg:
       "It’s been a pleasure working with you on the PWA and the BX features. I will miss you very much.",
     coverImg: "./jennycover.jpg",
-    profileImg: "./jennyprofile.jpg"
+    profileImg: "./jennyprofile.jpg",
   },
   {
     title: "Er..",
@@ -137,7 +137,7 @@ export const messages = [
     msg:
       "so you anyhow write on the 天灯? Great knowing you at Carousell! Don't keep in touch!",
     coverImg: "./winstoncover.jpg",
-    rel: "Ex-housemate"
+    rel: "Ex-housemate",
   },
   {
     name: "stacey",
@@ -146,14 +146,14 @@ export const messages = [
       "Thanks for all the fun durian / f45 / ~web~ SWAT team times! See you around in the East",
     coverImg: "./staceycover.jpg",
     profileImg:
-      "https://avatars0.githubusercontent.com/u/3874336?s=460&u=eb3cdc70874036f6da0c72ff58253e3082b86405&v=4"
+      "https://avatars0.githubusercontent.com/u/3874336?s=460&u=eb3cdc70874036f6da0c72ff58253e3082b86405&v=4",
   },
   {
     name: "yamato chua",
     title: "Plz explained y u leave??",
     msg: "Good AM… Tks n rgds…",
     coverImg: "./yaohuicover.jpg",
-    profileImg: "./yaohuiprofile.jpg"
+    profileImg: "./yaohuiprofile.jpg",
   },
   {
     name: "Dylan",
@@ -161,7 +161,7 @@ export const messages = [
     msg:
       "I've heard it said, that people come into our lives, for a reason, bringing something we must learn; and we are led to those who help us most to grow; if we let them, and we help them in return.",
     coverImg: "./dylancover.jpg",
-    profileImg: "./dylanprofile.jpg"
+    profileImg: "./dylanprofile.jpg",
   },
   {
     name: "Yu Sheng",
@@ -169,13 +169,13 @@ export const messages = [
     msg:
       "Hi Miss Chia!! Omg how can this be allow?! I just realised we didn’t take any photos together! Also does it mean if we remain friends I can get some food discounts or sth, asking for a friend.",
     coverImg: "./yushengcover.jpg",
-    profileImg: "./yushengprofile.jpg"
+    profileImg: "./yushengprofile.jpg",
   },
   {
     name: "Ben",
     title: "first PR",
     msg: "ty for your contributions and fun times! keep in touch!!",
-    coverImg: "./bencover.png"
+    coverImg: "./bencover.png",
   },
   {
     name: "Victor",
@@ -183,7 +183,7 @@ export const messages = [
     msg:
       "This lady here cannot be found in office! If found please call and bring her back :pepesad:",
     coverImg: "./viccover.jpg",
-    profileImg: "./vicprofile.jpg"
+    profileImg: "./vicprofile.jpg",
   },
   {
     name: "cyrandy",
@@ -191,21 +191,21 @@ export const messages = [
     msg:
       "sad i nv got my chance back to work with you again T_T stay in touch and let's have more #THEBACKYARDBAKERS date ❤️",
     coverImg: "./cyrandyprofile.jpg",
-    profileImg: "./cyrandycover.jpg"
+    profileImg: "./cyrandycover.jpg",
   },
   {
     name: "chenghua",
     title: "BYE BYE GOD-LIKE BOSS",
     msg: "So glad to be working with you. Best wishes for your next journey.",
     coverImg: "./chcover.jpg",
-    profileImg: "./chprofile.png"
+    profileImg: "./chprofile.png",
   },
   {
     name: "jessica",
     title: "Dear cheesey potato",
     msg: "This is the end of our reign. Say no to nepotism.",
     coverImg: "./jessicacover.jpg",
-    profileImg: "./jessicaprofile.jpg"
+    profileImg: "./jessicaprofile.jpg",
   },
   {
     name: "rita",
@@ -213,6 +213,6 @@ export const messages = [
     msg:
       "I have so many funny video of you LOL. Thank you for everything you have done for Carousell and the people you have worked with. THE BEST PM EVER and the cutest and funniest gal.",
     coverImg: "./ritacover.jpg",
-    profileImg: "./ritaprofile.png"
-  }
+    profileImg: "./ritaprofile.png",
+  },
 ];
